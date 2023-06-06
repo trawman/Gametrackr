d4debd2f1c0adc1f0ab172df3b0d19addd0d058b
trawman <danielegal142@gmail.com>
2023-06-06 10:24:22 -0300

    nodejs novo

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-
-const app = express();
-
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname)));
-
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/index.html') {
+  if (req.url === '/') {
     if (req.headers.cookie) {
       const darkModeCookie = req.headers.cookie
         .split(';')
@@ -20,26 +10,202 @@ const server = http.createServer((req, res) => {
 
       if (darkModeCookie) {
         const isDarkMode = darkModeCookie.split('=')[1] === 'true';
-        if (isDarkMode) {
-          res.setHeader('Set-Cookie', 'darkMode=true; Path=/');
-        } else {
-          res.setHeader('Set-Cookie', 'darkMode=false');
-        }
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="keywords" content="game,play,web">
+        
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
+            <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
+            <link rel="stylesheet" href="MediaQuery.css">
+            <title>GameTrackr</title>
+        
+        </head>
+        
+        <body${isDarkMode ? ' class="dark-mode"' : ''}>
+          <div id="top">
+            <ul>
+              <li><a href="index.html">Home</a></li>
+            </ul>
+            <img src="imagens/home.svg">
+            <button id="dark-mode">
+              <img src="imagens/dark.svg">
+            </button>
+          </div>
+          <div class="container">
+            <div id="mid">
+              <div class="picture">
+                <img src="imagens/lol.jpg">
+              </div>
+              <div class="picture">
+                <img src="imagens/fortnite.jpg">
+              </div>
+              <div class="picture">
+                <img src="imagens/eurot.jpg">
+              </div>
+              <div class="picture">
+                <img src="imagens/gta.jpg">
+              </div>
+              <div class="picture">
+                <img src="imagens/Fall-Guys.jpg">
+              </div>
+              <div class="picture">
+                <img src="imagens/among.jpg">
+              </div>
+              <div class="picture">
+                <img src="imagens/cod.jpg">
+              </div>
+              <div class="picture">
+                <img src="imagens/genshin.jpg">
+              </div>
+              <div class="picture">
+                <img src="imagens/valorant.jpg">
+              </div>
+            </div>
+            <div id="bottom">
+              <ul>
+                <li><a href="sobre.html">Sobre</a></li>
+                <li><a href="contato.html">Contato</a></li>
+              </ul>
+            </div>
+          </div>
+          
+          <script>
+          const button = document.getElementById('dark-mode');
+          const body = document.body;
+        
+          button.addEventListener('click', function() {
+            body.classList.toggle('dark-mode');
+            // Salva o estado do tema escuro no cookie
+            const isDarkMode = body.classList.contains('dark-mode');
+            document.cookie = `darkMode=${isDarkMode}`;
+          });
+        
+          // Verifica se existe um cookie para o tema escuro
+          const darkModeCookie = document.cookie
+            .split(';')
+            .map(cookie => cookie.trim())
+            .find(cookie => cookie.startsWith('darkMode='));
+        
+          if (darkModeCookie) {
+            const isDarkMode = darkModeCookie.split('=')[1] === 'true';
+            // Aplica o tema escuro se o cookie estiver definido como true
+            if (isDarkMode) {
+              body.classList.add('dark-mode');
+            }
+          }
+          </script>
+        </body>
+        
+        </html>`);
+        res.end();
+        return;
       }
     }
-
-    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        res.end('Internal Server Error');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
+    
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="game,play,web">
+    
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
+        <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="MediaQuery.css">
+        <title>GameTrackr</title>
+    
+    </head>
+    
+    <body>
+      <div id="top">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+        </ul>
+        <img src="imagens/home.svg">
+        <button id="dark-mode">
+          <img src="imagens/dark.svg">
+        </button>
+      </div>
+      <div class="container">
+        <div id="mid">
+          <div class="picture">
+            <img src="imagens/lol.jpg">
+          </div>
+          <div class="picture">
+            <img src="imagens/fortnite.jpg">
+          </div>
+          <div class="picture">
+            <img src="imagens/eurot.jpg">
+          </div>
+          <div class="picture">
+            <img src="imagens/gta.jpg">
+          </div>
+          <div class="picture">
+            <img src="imagens/Fall-Guys.jpg">
+          </div>
+          <div class="picture">
+            <img src="imagens/among.jpg">
+          </div>
+          <div class="picture">
+            <img src="imagens/cod.jpg">
+          </div>
+          <div class="picture">
+            <img src="imagens/genshin.jpg">
+          </div>
+          <div class="picture">
+            <img src="imagens/valorant.jpg">
+          </div>
+        </div>
+        <div id="bottom">
+          <ul>
+            <li><a href="sobre.html">Sobre</a></li>
+            <li><a href="contato.html">Contato</a></li>
+          </ul>
+        </div>
+      </div>
+      
+      <script>
+      const button = document.getElementById('dark-mode');
+      const body = document.body;
+    
+      button.addEventListener('click', function() {
+        body.classList.toggle('dark-mode');
+        // Salva o estado do tema escuro no cookie
+        const isDarkMode = body.classList.contains('dark-mode');
+        document.cookie = `darkMode=${isDarkMode}`;
+      });
+    
+      // Verifica se existe um cookie para o tema escuro
+      const darkModeCookie = document.cookie
+        .split(';')
+        .map(cookie => cookie.trim())
+        .find(cookie => cookie.startsWith('darkMode='));
+    
+      if (darkModeCookie) {
+        const isDarkMode = darkModeCookie.split('=')[1] === 'true';
+        // Aplica o tema escuro se o cookie estiver definido como true
+        if (isDarkMode) {
+          body.classList.add('dark-mode');
+        }
       }
-    });
-  }
-});
-
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
-});
+      </script>
+    </body>
+    
+    </html>`);
+    res.end();
+  });
+  
+  server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
