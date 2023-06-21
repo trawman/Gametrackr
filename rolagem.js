08413115b45bd5d93c984db5a6af0ba6c38e1398
trawman <danielegal142@gmail.com>
2023-06-20 23:14:13 -0300

    alterado a base do html trocando div id pra class colocado efeito smooth na pagina e no botao de seta segue bugado com as animaçoes sumindo tudo media queries principal erro consertar as divs fodase tudo e todos

--- rolagem.js
+++ rolagem.js
@@ -5,3 +5,32 @@ ScrollReveal().reveal('.container', {
     origin: 'bottom',
     reset: true
   });
+  
+function scrollToSection(target) {
+  const element = document.querySelector(target);
+  window.scrollTo({
+    top: element.offsetTop,
+    behavior: 'smooth'
+  });
+}
+
+const setaImagem = document.querySelector('.seta');
+setaImagem.addEventListener('click', function() {
+  scrollToSection('#bottom');
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  var setaImg = document.getElementById("seta-img");
+
+ 
+  setaImg.addEventListener("click", function(event) {
+   
+    event.preventDefault();
+
+   
+    var bottomElement = document.getElementById("bottom");
+    bottomElement.scrollIntoView({ behavior: "smooth" });
+  });
+});
