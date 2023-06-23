d820510b66951731df6433771c2e22421aabc04d
trawman <danielegal142@gmail.com>
2023-06-23 10:19:26 -0300

    feito popup de privacidade para a pagina inicial, e remoção de bugs

--- cookie.js
+++ cookie.js
@@ -1,26 +1,3 @@
-const button = document.getElementById('dark-mode');
-  const body = document.body;
-
-  button.addEventListener('click', function() {
-    body.classList.toggle('dark-mode');
-  
-    const isDarkMode = body.classList.contains('dark-mode');
-    document.cookie = `darkMode=${isDarkMode}`;
-  });
-
-  const darkModeCookie = document.cookie
-    .split(';')
-    .map(cookie => cookie.trim())
-    .find(cookie => cookie.startsWith('darkMode='));
-
-  if (darkModeCookie) {
-    const isDarkMode = darkModeCookie.split('=')[1] === 'true';
-
-    if (isDarkMode) {
-      body.classList.add('dark-mode');
-    }
-  }
-
   function showPopup() {
     var popup = document.getElementById("popup");
     popup.style.display = "block";
